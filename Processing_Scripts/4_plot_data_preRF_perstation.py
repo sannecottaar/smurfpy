49122dbaa8310acc73ccbc53718d22471ec9d953
stephenpugh <sdp43@cam.ac.uk>
2020-08-12 10:59:54 +0100

    Update 4_plot_data_preRF_perstation.py
    Spelling and capitalisation.

--- Processing_Scripts/4_plot_data_preRF_perstation.py
+++ Processing_Scripts/4_plot_data_preRF_perstation.py
@@ -3,7 +3,6 @@
 # This script plots ZRT seismogram components with predicted arrival times
 # after initial processing and before RF creation
 
-#
 
 from obspy import read
 import matplotlib.pyplot as plt
@@ -11,31 +10,27 @@ import glob
 import numpy as np
 
 direc = '../Data'
-
-
 stadirs = glob.glob(direc + '/*')
-
 
 for stadir in stadirs:
 
-    # loop through events
+    # Loop through events
     stalist = glob.glob(stadir + '/*.PICKLE')
 
-    c = 0
     # Loop through data
     if(len(stalist) > 0):
-        for i in range(0, len(stalist),20):  # Plottin every 20
+        for i in range(0, len(stalist),20):  # Plotting every 20
                 print(i)
                 seis = read(stalist[i], format='PICKLE')
 
-                # extract epi dist of event/station from header info
+                # Extract epi dist of event/station from header info
                 distdg = seis[0].stats['dist']
 
                 #----------------PLOT VERTICAL--------------------------------
                 # 1 of 3 subplots (grid 1 row 3 columns)
                 plt.subplot(1, 3, 1)
                 vertical = seis.select(channel='*HZ')[
-                    0]  # extract V from stream
+                    0]  # Extract V from stream
                 vertical.filter(
                     'bandpass',
                     freqmin=0.01,
@@ -46,7 +41,7 @@ for stadir in stadirs:
                 plt.plot(
                     vertical.times(),
                     vertical.data / np.max(vertical.data) + np.round(distdg),
-                    'k')  # plot normalised data as black line
+                    'k')  # Plot normalised data as black line
 
                 # Plot predicted travel times as coloured points (add more
                 # phases as wanted)
@@ -59,8 +54,8 @@ for stadir in stadirs:
                     np.round(distdg),
                     '.g')
 
-                # lable plot
-                plt.title('Vertial')
+                # Label plot
+                plt.title('Vertical')
                 plt.ylabel('Epi dist (degrees)')
                 plt.xlabel('Time from Origin (sec)')
 
