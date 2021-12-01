82a9c27388d30d3c93a01f6c42d7841ee3168351
Sophia <72014836+sophiakibaker@users.noreply.github.com>
2021-12-01 11:06:53 +0000

    Axis limits added

--- Plotting_Scripts/plot_map_pierce_points.py
+++ Plotting_Scripts/plot_map_pierce_points.py
@@ -65,6 +65,7 @@ lon_0 = (lonmin+lonmax)/2
 lat_0 = (latmin+latmax)/2
 
 m = plt.axes(projection=ccrs.Mercator())
+m.set_extent([lonmin,lonmax,latmin,latmax], crs=ccrs.PlateCarree())
 
 m.add_feature(cfeature.COASTLINE)
 m.add_feature(cfeature.LAND, color="lightgrey", alpha=0.5)
